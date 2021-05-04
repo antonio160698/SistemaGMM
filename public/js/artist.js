9e0d4404c804b28b525190382f370841bb54b579
antonio160698 <antonio.1606.98@gmail.com>
2021-05-04 00:34:42 -0500

    nombres de artistas

--- public/js/artist.js
+++ public/js/artist.js
@@ -8,9 +8,11 @@ window.onload = ()=>{
 			if(data.data){
                 let cards = '';
                 for(let i=0; i < data.data.length; i++){
-                    cards += '<div class="col-md-3 p-0"><img class="w-100" style="height:280px;" src="'+data.data[i].Image+'"/></div>';
+                    cards += '<div class="col-md-3 p-0 w-100" style="height:280px;background-image:url(\''+data.data[i].Image+'\');background-size: cover;"><a ><h3>'+data.data[i].Nickname+'</h3></a></div>';
                 }
                 $('#ListArtist').append(cards);
+                $('#ListArtist div').mouseover((e)=>{e.currentTarget.children[0].style.display = "block";});
+                $('#ListArtist div').mouseout((e)=>{e.currentTarget.children[0].style.display = "none";})
             }
 		}
 	})
